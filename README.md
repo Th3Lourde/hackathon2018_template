7338dd5a3be6e1d58ac522eafbf94e53236ae08b
Luis <lfr4704@gmail.com>
2018-04-25 23:48:14 -0700

    revised files

---
+++ README.md
@@ -0,0 +1,12 @@
+# Th3Lourde.github.io
+Website Created During 2018 Hackathon using Html, Css and Js
+
+People who contributed to this project:
+
+Ahmad Usama  LinkedIn: https://www.linkedin.com/in/ahmad-usama-33b33788/
+
+Curtis Phan LinkedIn: https://www.linkedin.com/in/curtisjphan/
+
+Luis Fernando R LinkedIn: https://www.linkedin.com/in/luis-robles-pe
+
+Thomas Machado LinkedIn: https://www.linkedin.com/in/thomaspmach
--- js/master.js
+++ js/master.js
@@ -4,10 +4,17 @@
 //     alert("Animate Up");
 //     $("#black-menu").animate({height: '10vw'}, 5, 'in-out')
 // });
+// Makes the menu move when clicked
+// Toggle for simplicity
 // var animateVar = true
 document.getElementById("animateUp").addEventListener("click", function() {
-    var elementToSlide = document.getElementById("mymenu");
-    document.getElementById("black-menu").classList.toggle("is-active");
-    $( "#mymenu" ).fadeToggle( "slow", "linear" );
+    // var elementToSlide = document.getElementById("mymenu");
+    var width = $(window).width();
+
+    // alert(width);
+    if (width >= 1025){
+      $( "#mymenu" ).fadeToggle( "slow", "linear" );
+      document.getElementById("black-menu").classList.toggle("is-active");
+    }
     // $('li').style
 });
